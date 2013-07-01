3ff944dbcadfd1f9886ecad28e9ecec90791048c
Jing Cheng <libraseacheng@gmail.com>
2013-07-01 14:29:47 -0500

    Added module with functions

--- add_nums.py
+++ add_nums.py
@@ -1,10 +1,8 @@
-
-
 # This has been edited by Jing and see what's going to change in the future
-def add_two(a,b):
-    return a+b
+from adding import add_two, add_three
 
 def main():
+    print ("2 + 2 + 2 = ", add_three(2,2,2))
     print ("2 + 3 = ", add_two(2,3))
 
 
