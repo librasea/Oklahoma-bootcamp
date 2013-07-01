9f17b9ee260e1a8d79a61bde9733b47393fc648d
Jing Cheng <libraseacheng@gmail.com>
2013-07-01 14:37:00 -0500

    Small tweaks to the code

--- add_nums.py
+++ add_nums.py
@@ -2,11 +2,12 @@
 
 # This has been edited by Jing and see what's going to change in the future
 def add_two(a,b):
+    print("Hi, i'm just adding two numbers here")
     return a+b
 
 def main():
     print ("2 + 3 = ", add_two(2,3))
-
+    print("4+4 = ", add_two(4,4))
 
 if __name__ = "__main__":
     main()
